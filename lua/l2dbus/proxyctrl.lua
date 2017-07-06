591c814cfe4b145763cd2af28bd0dfb0f9d5a2c7
Glenn Schmottlach <gschmottlach-xse@users.noreply.github.com>
2017-07-06 08:37:32 -0400

    Issue #9: Incorrect DBus property setter signature
    Incorporated suggested patch by user sfyang. See Issue #9.

--- lua/l2dbus/proxyctrl.lua
+++ lua/l2dbus/proxyctrl.lua
@@ -1029,7 +1029,7 @@ newPropertyProxy = function(proxyCtrl, metadata)
 			
 			msg:addArgsBySignature("s", metadata.interface)
 			msg:addArgsBySignature("s", propName)
-			msg:addArgs(l2dbus.DbusTypes.Variant.new(value), "v" .. propInfo.sig)
+			msg:addArgsBySignature("v", value)
 			local reply = nil
 			local errName = nil
 			local errMsg = nil
